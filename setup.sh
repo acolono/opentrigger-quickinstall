d2879a0e34df7aa7dce60c84cc7f7c6a4cfe1bf2
Flo <f+git@fida.biz>
2016-10-15 19:23:21 +0200

    dynamic url

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-## wget -qO- https://get-ot.3g1l.com/repositories.sh | bash
+## wget -qO- https://piccaso.github.io/opentrigger/setup.sh | bash
 
 export DEBIAN_FRONTEND=noninteractive
 
@@ -8,7 +8,10 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 
 sudo wget -O /etc/apt/sources.list.d/mosquitto.list http://repo.mosquitto.org/debian/mosquitto-jessie.list
-wget -O- http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key | sudo apt-key add -
+wget -qO- http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key | sudo apt-key add -
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 echo "deb http://dl.bintray.com/ao/opentrigger jessie main" | sudo tee /etc/apt/sources.list.d/opentrigger.list
+
+apt-get update
+apt-get install -y opentrigger
